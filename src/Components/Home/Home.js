2520fd5cbfce58df599680c99c04a22b84b62da3
Azer Ben Azzouz <32598330+Azerbenazzouz@users.noreply.github.com>
2022-01-21 23:52:41 +0100

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <React.Fragment>
     <div class="container pt-5 pb-5 text-center">
       <div class="row">
-        <div class="col-lg" ><img src="./img/mee.jpg" class="rounded-circle pt-5 mt-5 mb-5  w-50" alt="..."/></div>
+        <div class="col-lg" ><img src="https://scontent.ftun18-1.fna.fbcdn.net/v/t39.30808-6/272256926_1011421272774583_7963598134085313868_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=FDjgwjuO1cEAX90sZK8&tn=KMhkKHeITm7ZyrJR&_nc_ht=scontent.ftun18-1.fna&oh=00_AT9xJ6k1u_mmLQvuiE8_8QJB3XBu5GkXs93PjukTG-0ldA&oe=61F07933" class="rounded-circle pt-5 mt-5 mb-5  w-50" alt="..."/></div>
         <div class="col-lg center">
         <p style={{textAlign:'center'}} className='fw-bold fs-1 pt-4 pb-2 '>
           Hello! I am <strong style={{color:'blue' ,fontSize:'40px'}}>Azer Ben Azzouz</strong><br/><strong style={{color:'orange',font:'bold' ,fontSize:'40px'}}>Visual Designer &amp;<br/> Front-end Developer</strong><br/> I work as interface and front end developer. <br/>I have passion for pixel perfect, <br/> minimal and easy to use interfaces.<br/>I design and develop responsive, <br/> optimized and efficient websites. Currently available for freelance opportunities.
